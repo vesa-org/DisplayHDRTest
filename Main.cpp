f45ab00936a22da33413ba09e19f36a135d8a6f7
Simon Tao <13thsymphony@gmail.com>
2020-05-17 22:04:49 -0700

    Add win32 app icon. Update to 19H1 SDK.

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,7 @@
 
 #include "pch.h"
 #include "Game.h"
+#include "resource.h"
 
 using namespace DirectX;
 
@@ -62,12 +63,12 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         wcex.cbClsExtra = 0;
         wcex.cbWndExtra = 0;
         wcex.hInstance = hInstance;
-        wcex.hIcon = LoadIcon(hInstance, L"IDI_ICON");
+        wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON));
         wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
         wcex.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
         wcex.lpszMenuName = nullptr;
         wcex.lpszClassName = L"DisplayHDRComplianceTestsWindowClass";
-        wcex.hIconSm = LoadIcon(wcex.hInstance, L"IDI_ICON");
+        wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_ICON));
         if (!RegisterClassEx(&wcex))
             return 1;
 
